ef0df8489bbdbc85a36894f8204ad593ee1c5553
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-26 00:32:49 +0400

    25.01 "Повторяем основы. Часть 1" Задача 2 "Функция same_parity()"

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-def hide_card(card):
-    card = card.replace(" ", "")
-    return "*" * 12 + card[-4:]
+# 1
+def same_parity(numbers):
+    return [i for i in numbers if numbers[0] % 2 == i % 2]
 
 
-card = '3456 9012 5678 1234'
-print(hide_card(card))
+# 2
+def same_parity2(numbers):
+    res = list(filter(lambda x: x % 2 == numbers[0] % 2, numbers))
+    return res
+
+
+print(same_parity([6, 0, 67, -7, 10, -20]))
+print(same_parity2([6, 0, 67, -7, 10, -20]))
