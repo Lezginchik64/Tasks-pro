f1cd35a6094eaa5bf26f68a80928dd6057f1ed16
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-26 00:57:51 +0400

    25.01 "Повторяем основы. Часть 1" Задача 3 "Функция is_valid()"

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 # 1
-def same_parity(numbers):
-    return [i for i in numbers if numbers[0] % 2 == i % 2]
+def is_valid(string):
+    return all((4 <= len(string) <= 6, string.isdigit()))
 
 
 # 2
-def same_parity2(numbers):
-    res = list(filter(lambda x: x % 2 == numbers[0] % 2, numbers))
-    return res
+def is_valid(string):
+    return string.isdigit() and len(string) in (4, 5, 6)
 
 
-print(same_parity([6, 0, 67, -7, 10, -20]))
-print(same_parity2([6, 0, 67, -7, 10, -20]))
+# 3
+def is_valid(string):
+    res = [i for i in string if 4 <= len(string) <= 6 and i.isdigit()]
+    return len(string) == len(res) and len(string) > 1
+
+
+print(is_valid('89abc1'))
+print(is_valid('4367'))
+print(is_valid('900876'))
+print(is_valid('49 83'))
+print(is_valid(""))
