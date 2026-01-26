a58c21b5cd0f855586540c7da8946a42a6804e48
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-27 01:56:14 +0400

    26.01 "Повторяем основы. Часть 1" Задача 11 "Функция get_biggest() 🌶️🌶️"

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-# 1
-def spell(*args):
-    return {k[0].lower(): len(k) for k in sorted(args, key=len)}
+def get_biggest(numbers):
+    if not numbers:
+        return -1
+    numbers = list(map(str, numbers))
+    numbers.sort(reverse=True, key=lambda x: x * 10)    # Мы умножаем СТРОКУ на 10, чтобы получить достаточно длинную строку для сравнения
+    return int("".join(numbers))          # Например, для "3" и "30":
+                                            # "3" * 10 = "3333333333"
+                                            # "30" * 10 = "30303030303030303030"
+                                                 # 33 > 30, поэтому 3 будет стоять перед 30
 
-
-# 2
-def spell(*args):
-    res = {}
-    for word in args:
-        key = word[0].lower()
-        if key not in res or len(word) > res[key]:
-            res[key] = len(word)
-    return res
-
-
-words = ['Россия', 'Австрия', 'Австралия', 'РумыниЯ', 'Украина', 'КИТай', 'УЗБЕКИСТАН']
-print(spell(*words))
-
-print(spell('Математика', 'История', 'химия', 'биология', 'Информатика'))
-
-words = ['fruit', 'football', 'February', 'forest', 'Family']
-print(spell(*words))
+print(get_biggest([1, 2, 3]))
+print(get_biggest([61, 228, 9, 3, 11]))
+print(get_biggest([7, 71, 72]))
