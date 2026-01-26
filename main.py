506e4165f06dd6ee4eded7b6856949ce56210c36
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-27 00:58:15 +0400

    26.01 "Повторяем основы. Часть 1" Задача 8 "Функция index_of_nearest()"

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-# 1
-def filter_anagrams(word, words):
-    return [i for i in words if sorted(word) == sorted(i)]
+def index_of_nearest(numbers, number):
+    return numbers.index(min(numbers, key=lambda x: abs(number - x))) if numbers else -1
 
 
-# 2
-def letters(words):
-    letters_dict = {}
-    for i in words:
-        letters_dict[i] = letters_dict.get(i, 0) + 1
-    return letters_dict
-
-def filter_anagrams(word, anagrams):
-    return [i for i in anagrams if letters(word) == letters(i)]
-
-
-print(filter_anagrams('отсечка', ['сеточка', 'стоечка', 'тесачок', 'чесотка']))
-
-word = 'abba'
-anagrams = ['aabb', 'abcd', 'bbaa', 'dada']
-print(filter_anagrams(word, anagrams))
+print(index_of_nearest([7, 13, 3, 5, 18], 0))
+print(index_of_nearest([9, 5, 3, 2, 11], 4))
