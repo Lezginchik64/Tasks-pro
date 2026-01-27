23f162d5a49df077a0add4236ea684e4f988f9c4
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-28 02:48:05 +0400

    27.01 "Повторяем основы. Часть 2" Задача 1 "Тимур, Артур и новый курс️"

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-def get_biggest(numbers):
-    if not numbers:
-        return -1
-    numbers = list(map(str, numbers))
-    numbers.sort(reverse=True, key=lambda x: x * 10)    # Мы умножаем СТРОКУ на 10, чтобы получить достаточно длинную строку для сравнения
-    return int("".join(numbers))          # Например, для "3" и "30":
-                                            # "3" * 10 = "3333333333"
-                                            # "30" * 10 = "30303030303030303030"
-                                                 # 33 > 30, поэтому 3 будет стоять перед 30
+def funk(d1, d2, d3):
+    m1 = d1 + d2 + d3
+    m2 = 2 * d1 + 2 * d2
+    m3 = 2 * d2 + 2 * d3
+    m4 = 2 * d1 + 2 * d3
+    return min(m1, m2, m3, m4)
 
-print(get_biggest([1, 2, 3]))
-print(get_biggest([61, 228, 9, 3, 11]))
-print(get_biggest([7, 71, 72]))
+
+d1, d2, d3 = int(input()), int(input()), int(input())
+print(funk(d1, d2, d3))
+
+# #1) ДОМ --> МАГАЗИН 1 -->  МАГАЗИН 2 --> ДОМ
+# #2) ДОМ --> МАГАЗИН 1 -->  МАГАЗИН 2 -->  МАГАЗИН 1 --> ДОМ
+# #3) ДОМ --> МАГАЗИН 2 -->  МАГАЗИН 1 -->  МАГАЗИН 2--> ДОМ
+# #4) ДОМ --> МАГАЗИН 1 -->  ДОМ --> МАГАЗИН 2 --> ДОМ
