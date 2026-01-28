d8c64638590c41b3508ec71c981fe724ccb94b7b
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-29 02:49:04 +0400

    28.01 "Повторяем основы. Часть 2" Задача 6 "Трудности перевода"

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-groups = {}
-for i in range(1, int(input()) + 1):
-    nums_sum = sum(map(lambda x: int(x), str(i)))
-    groups[nums_sum] = groups.get(nums_sum, 0) + 1
-print(max(groups.values()))
-# ключ словаря groups - это сумма цифр
-# значение - сколько чисел имеет такую сумму цифр
+# 1
+n = int(input())
+langs = set(input().split(', '))
+for _ in range(n - 1):      # Мы уже считали первого человека, осталось ещё n-1.
+    langs &= set(input().split(', '))       # &= — это оператор пересечения множеств.
+                                            # Он оставляет в langs только те элементы, которые есть и в предыдущем множестве, и в новом.
+if langs:
+    print(*sorted(langs), sep=', ')
+else:
+    print("Сериал снять не удастся")
+
+# 2
+d = {}
+n = int(input())
+for _ in range(n):
+    lang = tuple(input().split(', '))
+    for i in lang:
+        d[i] = d.get(i, 0) + 1
+
+res = {k for k, v in d.items() if v == n}
+if res:
+    print(", ".join(sorted(res)))
+else:
+    print("Сериал снять не удастся")
