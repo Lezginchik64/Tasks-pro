e463fc9f7f463ac4fa48205c42ab1ad4db53690a
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-29 01:24:02 +0400

    28.01 "Повторяем основы. Часть 2" Задача 5 "Максимальная группа"

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-# 1
-n = input().split()
-num = set(int(i) for i in n if n.count(i) > 1)
-print(*sorted(num))
-
-# 2
-nums = [int(i) for i in input().split()]
-print(*sorted(filter(lambda x: nums.count(x) > 1, set(nums))))
+groups = {}
+for i in range(1, int(input()) + 1):
+    nums_sum = sum(map(lambda x: int(x), str(i)))
+    groups[nums_sum] = groups.get(nums_sum, 0) + 1
+print(max(groups.values()))
+# ключ словаря groups - это сумма цифр
+# значение - сколько чисел имеет такую сумму цифр
