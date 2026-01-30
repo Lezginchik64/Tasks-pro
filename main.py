dd4f84d3954cfe7ea8c7cab4d0a07cf1c9f1fbd6
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-31 01:21:01 +0400

    30.01 "Повторяем основы. Часть 2" Задача 7 "Схожие слова 🌶️"

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
-# 1
-n = int(input())
-langs = set(input().split(', '))
-for _ in range(n - 1):      # Мы уже считали первого человека, осталось ещё n-1.
-    langs &= set(input().split(', '))       # &= — это оператор пересечения множеств.
-                                            # Он оставляет в langs только те элементы, которые есть и в предыдущем множестве, и в новом.
-if langs:
-    print(*sorted(langs), sep=', ')
-else:
-    print("Сериал снять не удастся")
+str_vov = 'а, у, о, ы, и, э, я, ю, ё, е'.replace(', ', '')
+word_index = [ind for ind, lit in enumerate(input()) if lit in str_vov]
 
-# 2
-d = {}
-n = int(input())
-for _ in range(n):
-    lang = tuple(input().split(', '))
-    for i in lang:
-        d[i] = d.get(i, 0) + 1
-
-res = {k for k, v in d.items() if v == n}
-if res:
-    print(", ".join(sorted(res)))
-else:
-    print("Сериал снять не удастся")
+for _ in range(int(input())):
+    word = input()
+    if [ind for ind, lit in enumerate(word) if lit in str_vov] == word_index:
+        print(word)
