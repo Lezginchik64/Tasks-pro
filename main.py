3a2d81172e9aa42c0c6073c9b05f133a88b79ee3
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-02 01:12:05 +0400

    01.02.26 "Типы данных date и time. Часть 2" Задача 4.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from datetime import time
+from datetime import date
 
-alarm = time(7, 30, 25)
+andrew = date(1992, 8, 24)
 
-print('Часы:', alarm.strftime('%H'))
-print('Минуты:', alarm.strftime('%M'))
-print('Секунды:', alarm.strftime('%S'))
+print(andrew.strftime('%Y-%m'))  # выводим дату в формате YYYY-MM
+print(andrew.strftime('%B (%Y)'))  # выводим дату в формате month_name (YYYY)
+print(andrew.strftime('%Y-%j'))  # выводим дату в формате YYYY-day_number
