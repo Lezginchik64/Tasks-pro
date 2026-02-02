8f0e6e769300caf6c233877dd6980c01e0d2ee1b
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-03 01:46:32 +0400

    02.02.26 "Типы данных date и time. Часть 2" Задача 6 "Отсортированные даты"

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from datetime import date
 
-# 1
-print(min(date.fromisoformat(input()) for _ in range(2)).strftime('%d-%m (%Y)'))
-
-# 2
-date = date.fromisoformat(input())
-date1 = date.fromisoformat(input())
-print(min(date, date1).strftime('%d-%m (%Y)'))
+dates = sorted([date.fromisoformat(input()) for _ in range(int(input()))])
+for i in dates:
+    print(i.strftime('%d/%m/%Y'))
