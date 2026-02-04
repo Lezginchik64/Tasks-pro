03936bfc3420baae8ba2579c60ba977999f3391d
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-05 00:41:59 +0400

    05.02 "Типы данных date и time. Часть 2" Задача 9 "Корректные даты" !

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 from datetime import date
 
 
+# 1
 def is_correct(day, month, year):
+    try:
+        date(year, month, day)
+        return True
+    except ValueError:
+        return False
+
+
+count = 0
+date1 = input()
+while date1 != 'end':
+    if is_correct(*map(int, date1.split('.'))):
+        count += 1
+        print("Корректная")
+    else:
+        print("Некорректная")
+    date1 = input()
+print(count)
+
+
+# 2
+# Этот вариант отличается от 1 тем, что тут я изменял работу самой функции is_correct()
+def is_correct(dates):
     while True:
         try:
-            date(year, month, day)
+            day, month, year = dates.split(".")
+            date(int(year), int(month), int(day))
             return True
         except:
             return False
 
 
-print(is_correct(31, 12, 2021))
-print(is_correct(31, 13, 2021))
+correct = 0
+data1 = input()
+while data1 != "end":
+    if is_correct(data1):
+        correct += 1
+        print("Корректная")
+    else:
+        print("Некорректная")
+    data1 = input()
+print(correct)
