c30ecd44a0ea99a65b0896a9047d03a993ec5bca
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-08 03:10:21 +0400

    07.02 "Тип данных datetime". Задача 6 "Дневник космонавта 🌶️"

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from datetime import datetime
 
-data = {'Дима': ('03.11.2021 09:31:18', '03.11.2021 11:41:28'),
-        'Геор': ('01.11.2021 09:03:04', '01.11.2021 12:40:35'),
-        'Анна': ('02.11.2021 04:41:54', '02.11.2021 05:39:40'),
-        'Илина': ('02.11.2021 01:36:40', '02.11.2021 04:48:27'),
-        'Герман': ('04.11.2021 07:51:19', '04.11.2021 09:53:53'),
-        'Руслан': ('01.11.2021 11:26:06', '01.11.2021 12:56:24'),
-        'Лера': ('03.11.2021 11:09:41', '03.11.2021 14:37:41'),
-        'Егор': ('03.11.2021 05:29:38', '03.11.2021 06:01:59'),
-        'Максим': ('05.11.2021 13:05:03', '05.11.2021 14:27:41'),
-        'Саша': ('03.11.2021 04:14:26', '03.11.2021 05:10:58'),
-        'Марина': ('05.11.2021 15:21:06', '05.11.2021 18:33:46')}
+# 1
+with open('diary.txt', 'r', encoding='utf-8') as f:
+    print("\n\n".join(
+        sorted(f.read().strip().split('\n\n'), key=lambda x: datetime.strptime(x.split('\n')[0], '%d.%m.%Y; %H:%M'))))
 
-frm = '%d.%m.%Y %H:%M:%S'
-print(min(data, key=lambda x: abs(datetime.strptime(data[x][1], frm) - datetime.strptime(data[x][0], frm))))
+# 2
+print('\n\n'.join(sorted(
+    open('diary.txt', encoding='utf-8').read().strip().split('\n\n'),
+    key=lambda b: datetime.strptime(b.split('\n')[0], '%d.%m.%Y; %H:%M')
+)))
+
+# 3
+notes = {}
+pattern = '%d.%m.%Y; %H:%M'
+
+with open('diary.txt', 'r', encoding='utf-8') as file:
+    diary = file.read().split('\n\n')   # Мы режем по пустой строке → получаем отдельные отчёты.
+    for note in diary:
+        data, text = note.split('\n', 1)    # разделить только ОДИН раз, после первого переноса строки остановиться.
+        data = datetime.strptime(data, pattern)
+        notes[data] = text
+
+for k, v in sorted(notes.items()):
+    print(k)
+    print(v)
+    print()
