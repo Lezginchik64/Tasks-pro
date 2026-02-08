3bdf95d705ed6e5e7f339c3ea6c47b733163b5c8
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-09 03:31:01 +0400

    08.02 "Тип данных datetime". Задача 7 "Функция is_available_date() 🌶️" !

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
-from datetime import datetime
+from datetime import datetime, date
 
-# 1
-with open('diary.txt', 'r', encoding='utf-8') as f:
-    print("\n\n".join(
-        sorted(f.read().strip().split('\n\n'), key=lambda x: datetime.strptime(x.split('\n')[0], '%d.%m.%Y; %H:%M'))))
 
-# 2
-print('\n\n'.join(sorted(
-    open('diary.txt', encoding='utf-8').read().strip().split('\n\n'),
-    key=lambda b: datetime.strptime(b.split('\n')[0], '%d.%m.%Y; %H:%M')
-)))
+def expand(s):
+    if '-' in s:
+        start_s, end_s = s.split('-')
+    else:
+        start_s = end_s = s
 
-# 3
-notes = {}
-pattern = '%d.%m.%Y; %H:%M'
+    start = datetime.strptime(start_s, '%d.%m.%Y').date().toordinal()
+    end = datetime.strptime(end_s, '%d.%m.%Y').date().toordinal()
+    return set(range(start, end + 1))
 
-with open('diary.txt', 'r', encoding='utf-8') as file:
-    diary = file.read().split('\n\n')   # Мы режем по пустой строке → получаем отдельные отчёты.
-    for note in diary:
-        data, text = note.split('\n', 1)    # разделить только ОДИН раз, после первого переноса строки остановиться.
-        data = datetime.strptime(data, pattern)
-        notes[data] = text
 
-for k, v in sorted(notes.items()):
-    print(k)
-    print(v)
-    print()
+def is_available_date(booked_dates, date_for_booking):
+    booked_set = set()
+    for i in booked_dates:
+        booked_set.update(expand(i))
+
+    date_for_booking_set = expand(date_for_booking)
+
+    return date_for_booking_set.isdisjoint(booked_set)
+
+
+dates = ['04.11.2021', '05.11.2021-09.11.2021']
+some_date = '01.11.2021'
+print(is_available_date(dates, some_date))
+
+dates = ['04.11.2021', '05.11.2021-09.11.2021']
+some_date = '01.11.2021-04.11.2021'
+print(is_available_date(dates, some_date))
+
+dates = ['04.11.2021', '05.11.2021-09.11.2021']
+some_date = '06.11.2021'
+print(is_available_date(dates, some_date))
+
+dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']
+some_date = '12.11.2021'
+print(is_available_date(dates, some_date))
