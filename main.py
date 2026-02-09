1d032a647a258cbaa447644c817a636e7294b4ea
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-10 00:33:19 +0400

    09.02 "Тип данных timedelta". Задача 4 "Количество секунд"

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from datetime import datetime, timedelta
 
-pattern, day = '%d.%m.%Y', timedelta(days=1)
+# 1
+h, m, s = map(int, input().split(':'))
+td = timedelta(hours=h, minutes=m, seconds=s)
+print(td.seconds)
+
+# 2
+pattern = '%H:%M:%S'
+start = datetime.strptime('00:00:00', pattern)
 data = datetime.strptime(input(), pattern)
-print((data - day).strftime(pattern))
-print((data + day).strftime(pattern))
+print(int((data - start).total_seconds()))
