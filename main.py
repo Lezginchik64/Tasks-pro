d53d296871251f133d587a09463af05195bc160d
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-10 20:57:34 +0400

    10.02 "Тип данных timedelta". Задача 7 "Продуктивность"

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-from datetime import date
+from datetime import datetime, timedelta
 
-
-# 1
-def num_of_sundays(year):
-    count = 0
-    start = date(year, 1, 1).toordinal()
-    end = date(year, 12, 31).toordinal()
-    for i in range(start, end + 1):
-        if date.fromordinal(i).weekday() == 6:
-            count += 1
-    return count
-
-
-# 2
-def num_of_sundays2(year):
-    return date(year, 12, 31).strftime('%U')
-# %U - Номер недели в году (неделя начинается с воскресенья). Неделя, предшествующая первому воскресенью, является нулевой.
-
-print(num_of_sundays(2000))
-print(num_of_sundays2(2001))
+dt = datetime.strptime(input(), '%d.%m.%Y')
+print(dt.strftime('%d.%m.%Y'))
+for i in range(2, 11):
+    dt += timedelta(days=i)
+    print(dt.strftime('%d.%m.%Y'))
