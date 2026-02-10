9ab9d667a0fadd2dfc0ccb7433223d4136919078
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-10 19:07:29 +0400

    10.02 "Тип данных timedelta". Задача 6 "Функция num_of_sundays()"

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from datetime import datetime, timedelta
+from datetime import date
+
 
 # 1
-print((datetime.strptime(input(), '%H:%M:%S') + timedelta(seconds=int(input()))).time())
+def num_of_sundays(year):
+    count = 0
+    start = date(year, 1, 1).toordinal()
+    end = date(year, 12, 31).toordinal()
+    for i in range(start, end + 1):
+        if date.fromordinal(i).weekday() == 6:
+            count += 1
+    return count
+
 
 # 2
-h, m, s = map(int, input().split(':'))
-td = timedelta(hours=h, minutes=m, seconds=s) + timedelta(seconds=int(input()))
-res = datetime(1, 1, 1) + td
-print(res.strftime('%H:%M:%S'))
+def num_of_sundays2(year):
+    return date(year, 12, 31).strftime('%U')
+# %U - Номер недели в году (неделя начинается с воскресенья). Неделя, предшествующая первому воскресенью, является нулевой.
+
+print(num_of_sundays(2000))
+print(num_of_sundays2(2001))
