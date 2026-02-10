333666730ac44d0330c44ecb8615b2ca2f371eff
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-11 01:31:29 +0400

    10.02 "Тип данных timedelta". Задача 8 "Соседние даты"

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
-dt = datetime.strptime(input(), '%d.%m.%Y')
-print(dt.strftime('%d.%m.%Y'))
-for i in range(2, 11):
-    dt += timedelta(days=i)
-    print(dt.strftime('%d.%m.%Y'))
+# 1
+dt = [datetime.strptime(i, '%d.%m.%Y') for i in input().split()]
+res = [abs(dt[i] - dt[i - 1]).days for i in range(1, len(dt))]
+print(res)
+
+# 2
+l = list(map(lambda x: datetime.strptime(x, "%d.%m.%Y"), input().split()))
+dif = [abs(v - l[i]).days for i, v in enumerate(l[1:])]
+print(dif)
