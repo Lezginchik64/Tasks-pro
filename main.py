995bd2bfdcede2b435f5539d4fc0330532c9a577
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-12 02:25:48 +0400

    11.02 "Решение задач". Задача 1.

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from datetime import datetime, timedelta
 
-pattern = '%H:%M'
-start = datetime.strptime(input(), pattern)
-end = datetime.strptime(input(), pattern)
+data = [('07:14', '08:46'),
+        ('09:01', '09:37'),
+        ('10:00', '11:43'),
+        ('12:13', '13:49'),
+        ('15:00', '15:19'),
+        ('15:58', '17:24'),
+        ('17:57', '19:21'),
+        ('19:30', '19:59')]
+# 1
+data = [datetime.strptime(i[1], '%H:%M') - datetime.strptime(i[0], '%H:%M') for i in data]
+print(sum(data, start=timedelta()).seconds // 60)
 
-while True:
-    end_lesson = start + timedelta(minutes=45)
-    if end_lesson > end:
-        break
-    print(f'{start.strftime(pattern)} - {end_lesson.strftime(pattern)}')
-    start = end_lesson + timedelta(minutes=10)
+# 2
+second = 0
+for i in data:
+    start, end = [datetime.strptime(x, '%H:%M') for x in i]
+    second += (end - start).total_seconds()
+print(int(second // 60))
