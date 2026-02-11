1774778a830a6df386255d4d214458cd4736ab00
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-12 01:59:48 +0400

    11.02 "Тип данных timedelta". Задача 10 "Реп по матеше"

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
-from datetime import datetime, date, timedelta
+from datetime import datetime, timedelta
 
+pattern = '%H:%M'
+start = datetime.strptime(input(), pattern)
+end = datetime.strptime(input(), pattern)
 
-# 1
-def fill_up_missing_dates(dates):
-    date_list = sorted([datetime.strptime(i, '%d.%m.%Y').toordinal() for i in dates])
-    start, end = date_list[0], date_list[-1]
-    return [date.fromordinal(i).strftime('%d.%m.%Y') for i in range(start, end + 1)]
-
-
-# 2
-def fill_up_missing_dates(dates):
-    pattern = '%d.%m.%Y'
-    dates = [datetime.strptime(i, pattern) for i in dates]
-    start, end = min(dates), max(dates)
-    days = (end - start).days
-    return [(start + timedelta(days=i)).strftime(pattern) for i in range(days + 1)]
-
-
-dates = ['01.11.2021', '07.11.2021', '04.11.2021', '03.11.2021']
-print(fill_up_missing_dates(dates))
-
-dates = ['01.11.2021', '04.11.2021', '09.11.2021', '15.11.2021']
-print(fill_up_missing_dates(dates))
-
-dates = ['20.07.2021', '16.05.2021', '19.01.2021', '18.11.2021', '17.10.2021', '15.03.2021']
-print(len(fill_up_missing_dates(dates)))
+while True:
+    end_lesson = start + timedelta(minutes=45)
+    if end_lesson > end:
+        break
+    print(f'{start.strftime(pattern)} - {end_lesson.strftime(pattern)}')
+    start = end_lesson + timedelta(minutes=10)
