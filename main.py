829234748bf0b2e9bb85939bf512d2d8e07a04b2
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-12 16:39:09 +0400

    12.02 "Решение задач". Задача 2 "Пятница, 13-е"

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-from datetime import datetime, timedelta
+from datetime import datetime as dt
 
-data = [('07:14', '08:46'),
-        ('09:01', '09:37'),
-        ('10:00', '11:43'),
-        ('12:13', '13:49'),
-        ('15:00', '15:19'),
-        ('15:58', '17:24'),
-        ('17:57', '19:21'),
-        ('19:30', '19:59')]
-# 1
-data = [datetime.strptime(i[1], '%H:%M') - datetime.strptime(i[0], '%H:%M') for i in data]
-print(sum(data, start=timedelta()).seconds // 60)
+d = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
 
-# 2
-second = 0
-for i in data:
-    start, end = [datetime.strptime(x, '%H:%M') for x in i]
-    second += (end - start).total_seconds()
-print(int(second // 60))
+for i in range(dt.min.toordinal(), dt.max.toordinal() + 1):
+    if dt.fromordinal(i).day == 13:
+        d[dt.fromordinal(i).weekday()] = d.get(dt.fromordinal(i).weekday(), 0) + 1
+
+print(*d.values(), sep='\n')
