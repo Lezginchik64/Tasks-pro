81f66cc86e0bdcc9d781f313b86340d95364a265
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-12 17:18:33 +0400

    12.02 "Решение задач". Задача 3 "Снова не успел"

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from datetime import datetime as dt
+from datetime import datetime, timedelta
 
-d = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+# 1
+now = datetime.strptime(input(), '%d.%m.%Y %H:%M')
+now_time = timedelta(hours=now.hour, minutes=now.minute)
+if now.weekday() < 5 and timedelta(hours=9) <= now_time < timedelta(hours=21):
+    print((timedelta(hours=21) - now_time).seconds // 60)
+elif now.weekday() > 5 and timedelta(hours=10) <= now_time < timedelta(hours=18):
+    print((timedelta(hours=18) - now_time).seconds // 60)
+else:
+    print("Магазин не работает")
 
-for i in range(dt.min.toordinal(), dt.max.toordinal() + 1):
-    if dt.fromordinal(i).day == 13:
-        d[dt.fromordinal(i).weekday()] = d.get(dt.fromordinal(i).weekday(), 0) + 1
-
-print(*d.values(), sep='\n')
+# 2
+now = datetime.strptime(input(), '%d.%m.%Y %H:%M')
+now_time = timedelta(hours=now.hour, minutes=now.minute)
+work_time = (timedelta(hours=9), timedelta(hours=21)) if now.weekday() in range(5) else (timedelta(hours=10),
+                                                                                         timedelta(hours=18))
+if now_time >= work_time[1] or now_time < work_time[0]:
+    print("Магазин не работает")
+else:
+    print((work_time[1] - now_time).seconds // 60)
