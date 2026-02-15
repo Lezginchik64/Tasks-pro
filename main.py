0648197a13d9a0e8dfd97fff75e119a5feb1a14b
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-16 01:27:57 +0400

    15.02 "Модуль time" Задача 1 "Функция calculate_it()"

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-import timeit, random
-
-l = [random.randrange(1, 100_000) for _ in range(100_000)]
+import time as t
 
 
-def funk(n):
-    return n ** 3
+def calculate_it(funk, *args):
+    start = t.monotonic()
+    return funk(*args), t.monotonic() - start
 
 
-t1 = timeit.timeit('[funk(i) for i in l]', globals=globals(), number=100)   # Код выполнится 100 раз → возвращается общее время этих 100 запусков.
-t2 = timeit.repeat('list(map(funk, l))', globals=globals(), number=100, repeat=10)
-# Делается 10 независимых замеров, в каждом замере код выполняется по 100 раз, возвращается список из 10 чисел
-t3 = timeit.timeit('[i**3 for i in l]', globals=globals(), number=100)
+def add(a, b, c):
+    t.sleep(3)
+    return a + b + c
 
-print(f"List funk: {t1}")
-print(f"Map: {t2}")
-print(f"Map_min: {min(t2)}")
-print(f"List: {t3}")
+
+print(calculate_it(add, 1, 2, 3))
