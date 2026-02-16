9ac78754471c671caa478eff6d4858a4bf9943c1
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-17 01:56:41 +0400

    16.02 "Модуль calendar" Задача 4 "Количество дней 😉"

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from datetime import datetime as dt
 import calendar
 
-# 1
-d = dt.strptime(input(), '%Y-%m-%d')
-print(list(calendar.day_name)[d.weekday()])     # В разы быстрее 2 способа
-
-# 2
-print(dt.strptime(input(), '%Y-%m-%d').strftime('%A'))
+d = dt.strptime(input(), '%Y %m')
+print(calendar.monthrange(d.year, d.month)[1])
