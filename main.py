9fd9da8acc4dfac4fdd6fdaf324f0cbc07ff24ca
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-17 01:40:02 +0400

    16.02 "Модуль calendar" Задача 2 "Календарь на месяц"

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from datetime import datetime as dt
 import calendar
 
-for i in range(int(input())):
-    print(calendar.isleap(int(input())))
+# 1
+d = dt.strptime(input(), '%Y %b')
+print(calendar.month(d.year, d.month))
+
+# 2
+year, month = input().split()
+year, month = int(year), list(calendar.month_abbr).index(month)     # month_abbr - список с сокращенными названиями месяцев
+calendar.prmonth(year, month)   # тоже самое, только работает без print
