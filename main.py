cfb2404248171e3649bf526368ea1173a07704a4
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-17 15:56:22 +0400

    17.02 "Модуль calendar" Задача 8 "Третий четверг месяца"

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
-from datetime import datetime, timedelta, date
+from datetime import datetime
 import calendar
 
-
-# 1
-def get_all_mondays(year):
-    mondays = []
-    for month in range(1, 13):
-        for week in calendar.monthcalendar(year, month):
-            monday = week[0]
-            if monday:
-                mondays.append(date(year, month, monday))
-    return mondays
-
-
-# # 2
-def get_all_mondays(year):
-    res = []
-    start = datetime(year, 1, 1)
-    while start.year == year:
-        if start.weekday() == 0:
-            res.append(start)
-        start += timedelta(days=1)
-    return [i.date() for i in res]
-
-
-print(get_all_mondays(2021))
+year = int(input())
+for month in range(1, 13):
+    for day in range(15, 22):       # перебираем дни 15-21 и проверяем какой из них Четверг
+        if calendar.weekday(year, month, day) == calendar.THURSDAY:
+            thursday = datetime(year, month, day)
+            print(thursday.strftime('%d.%m.%Y'))
+            break
+# первый четверг: 1-7
+# второй четверг: 8-14
+# третий: 15-21 - именно этот диапазон проверяется (середина месяца), в нем гарантировано будет Четверг
