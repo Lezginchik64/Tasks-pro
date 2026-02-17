e4e2e7fe5a8c88ad043bf6fe728f6a09bfeeda78
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-17 14:37:01 +0400

    17.02 "Модуль calendar" Задача 6 "Функция get_all_mondays()"

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-from datetime import date
+from datetime import datetime, timedelta, date
 import calendar
 
 
 # 1
-def get_days_in_month(year, month):
-    month_num = list(calendar.month_name).index(month)
-    return [date(year, month_num, day) for day in range(1, calendar.monthrange(year, month_num)[1] + 1)]
+def get_all_mondays(year):
+    mondays = []
+    for month in range(1, 13):
+        for week in calendar.monthcalendar(year, month):
+            monday = week[0]
+            if monday:
+                mondays.append(date(year, month, monday))
+    return mondays
 
 
-# 2
-def get_days_in_month(year, month):
-    month_num = list(calendar.month_name).index(month)
-    count_day = calendar.monthrange(year, month_num)[1]
-    start = date(year, month_num, 1).toordinal()
-    end = date(year, month_num, count_day).toordinal()
-    return sorted([date.fromordinal(i) for i in range(start, end + 1)])
+# # 2
+def get_all_mondays(year):
+    res = []
+    start = datetime(year, 1, 1)
+    while start.year == year:
+        if start.weekday() == 0:
+            res.append(start)
+        start += timedelta(days=1)
+    return [i.date() for i in res]
 
 
-print(get_days_in_month(2021, 'December'))
+print(get_all_mondays(2021))
