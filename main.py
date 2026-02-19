6ca83461278808cec3534ce5e5868aaf647a5a6e
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-20 02:00:49 +0400

    19.02 "Потоковый ввод stdin и вывод stdout" Задача 7 "Панорамное агентство 🌶️" !

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import sys
 
 # 1
-print(*[i for i in sys.stdin if not i.lstrip().startswith('#')], sep='')
+d = {}
+l = [i for i in sys.stdin]
+for line in l[:-1]:
+    news, cat, true = line.strip().split(' / ')
+    d.setdefault(cat, []).append((news, true))
+category = l[-1].strip()
+if category in d:
+    print(*[i[0] for i in sorted(d[category], key=lambda x: (x[1], x[0]))], sep='\n')
 
 # 2
-[sys.stdout.write(i) for i in sys.stdin if not i.lstrip().startswith('#')]
+news = [i.strip().split(' / ') for i in sys.stdin]
+true_news = list(filter(lambda x: x[1] == news[-1][0], news[:-1]))
+print(*[i[0] for i in sorted(true_news, key=lambda x: (float(x[2]), x[0]))], sep='\n')
