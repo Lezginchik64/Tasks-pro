ce2b5e10241e19db717aba443711f95bb26d634e
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-20 14:32:53 +0400

    20.02 "Потоковый ввод stdin и вывод stdout" Задача 8 "Это точно Python? 🌶️"

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 import sys
+from datetime import datetime
 
-# 1
-num = [int(i) for i in sys.stdin]
-a = map(lambda a, b: b - a, num, num[1:])
-b = map(lambda a, b: b // a, num, num[1:])
-if len(set(a)) == 1:
-    print("Арифметическая прогрессия")
-elif len(set(b)) == 1:
-    print("Геометрическая прогрессия")
-else:
-    print("Не прогрессия")
+dates = [datetime.strptime(i.strip(), '%d.%m.%Y') for i in sys.stdin if i != '\n']
 
-# 2
-num = [int(i.strip()) for i in sys.stdin]
-arif = set([num[i] - num[i - 1] for i in range(1, len(num))])
-geom = set([num[i] // num[i - 1] for i in range(1, len(num))])
-if len(arif) == 1:
-    print("Арифметическая прогрессия")
-elif len(geom) == 1:
-    print("Геометрическая прогрессия")
+if sorted(set(dates)) == dates:
+    print("ASC")
+elif sorted(set(dates), reverse=True) == dates:
+    print("DESC")
 else:
-    print("Не прогрессия")
+    print("MIX")
