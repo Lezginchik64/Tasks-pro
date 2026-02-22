1b676e97011537d00ef56bd966f1a972e4788af5
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-23 01:46:24 +0400

    22.02 "Работа с csv-файлами" Задача 5 "Сортировка по столбцу"

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 import csv
 
-# 1
-with open('salary_data.csv', encoding="utf-8") as file:
-    reader = csv.reader(file, delimiter=';')
-    next(reader)
-    d = {}
-    for i in reader:
-        d.setdefault(i[0], []).append(int(i[1]))
-    print(*sorted(d, key=lambda x: sum(d[x]) // len(d[x])), sep='\n')
-
-# 2
-with open('salary_data.csv', encoding="utf-8") as file:
-    reader = list(csv.reader(file, delimiter=';'))
-    d = {}
-    for k, v in reader[1:]:
-        d[k] = d.get(k, []) + [int(v)]
-    print(*sorted(d, key=lambda x: sum(d[x]) // len(d[x])), sep='\n')
+with open('deniro.csv', encoding="utf-8") as file:
+    reader = list(csv.reader(file))
+    n = int(input()) - 1
+    reader_sorted = sorted(reader, key=lambda x: int(x[n]) if x[n].isdigit() else x[n])
+    for i in reader_sorted:
+        print(*i, sep=',')
