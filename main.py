75b78deb07e778777079e27eb0acf25c3a8690dc
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-24 01:50:07 +0400

    23.02 "Работа с csv-файлами" Задача 9 "Последний день на «Титанике»"

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import csv
 
-d = {}
-with open('wifi.csv', encoding="utf-8") as file_in:
-    reader = csv.DictReader(file_in, delimiter=';')
+# 1
+people = []
+with open('titanic.csv', encoding="utf-8") as file:
+    reader = csv.reader(file, delimiter=';')
+    next(reader)
     for row in reader:
-        reg = row['district']
-        num = int(row['number_of_access_points'])
-        d[reg] = d.get(reg, 0) + num
-sorted_dict = sorted(d.items(), key=lambda x: (-x[1], x[0]))     # -x[1] - аналог реверса
-print(*[f'{i[0]}: {i[1]}' for i in sorted_dict], sep='\n')
+        if int(row[0]) == 1 and float(row[3]) < 18:
+            people.append(row)
+people.sort(key=lambda x: x[2], reverse=True)
+for i in people:
+    print(i[1])
+
+# 2
+with open('titanic.csv', encoding='utf-8') as f:
+    passengers = [d for s, *d, a in csv.reader(f, delimiter=';') if s == '1' and float(a) < 18]
+    [print(name) for name, _ in sorted(passengers, key=lambda x: x[1], reverse=True)]
