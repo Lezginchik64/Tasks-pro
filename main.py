2e7e87fe1f8a218c14b19a2bbf525cf4ba56bd44
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-25 02:28:58 +0400

    24.02 "Работа с csv-файлами" Задача 10 "Лог-файл"

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import csv
+from datetime import datetime
 
-# 1
-people = []
-with open('titanic.csv', encoding="utf-8") as file:
-    reader = csv.reader(file, delimiter=';')
-    next(reader)
-    for row in reader:
-        if int(row[0]) == 1 and float(row[3]) < 18:
-            people.append(row)
-people.sort(key=lambda x: x[2], reverse=True)
-for i in people:
-    print(i[1])
+with open('name_log.csv', encoding='utf-8') as file:
+    header, *rows = csv.reader(file)
 
-# 2
-with open('titanic.csv', encoding='utf-8') as f:
-    passengers = [d for s, *d, a in csv.reader(f, delimiter=';') if s == '1' and float(a) < 18]
-    [print(name) for name, _ in sorted(passengers, key=lambda x: x[1], reverse=True)]
+d = {i[1]: i for i in sorted(rows, key=lambda x: datetime.strptime(x[2], '%d/%m/%Y %H:%M'))}
+
+with open('new_name_log.csv', 'w', encoding='utf-8', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(header)
+    writer.writerows(sorted(d.values(), key=lambda x: x[1]))
