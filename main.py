b4284249d108595ed1e2ade745d65bf07428b0b1
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-02-26 21:17:45 +0400

    26.02 "Работа с json-файлами. Часть 2" Задача 4.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import json
 
-club1 = {"name": "FC Byern Munchen", "country": "Germany", "founded": 1900,
-         "trainer": "Julian Nagelsmann", "goalkeeper": "M. Neuer", "league_position": 1}
+specs = {
+    'Модель': 'AMD Ryzen 5 5600G',
+    'Год релиза': 2021,
+    'Сокет': 'AM4',
+    'Техпроцесс': '7 нм',
+    'Ядро': 'Cezanne',
+    'Объем кэша L2': '3 МБ',
+    'Объем кэша L3': '16 МБ',
+    'Базовая частота': '3900 МГц'
+}
 
-club2 = {"name": "FC Barcelona", "country": "Spain", "founded": 1899,
-         "trainer": "Xavier Creus", "goalkeeper": "M. Ter Stegen", "league_position": 7}
-
-club3 = {"name": "FC Manchester United", "country": "England", "founded": 1878,
-         "trainer": "Michael Carrick", "goalkeeper": "D. De Gea", "league_position": 8}
-l = [club1, club2, club3]
-
-with open('data.json', 'w', encoding='utf-8') as file:
-    json.dump(l, file, indent=3)
+specs_json = json.dumps(specs, indent=3, ensure_ascii=False)
+print(specs_json)
