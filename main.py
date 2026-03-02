7feb9b237195e0e5ce5c5b15f548cd0bef3ac681
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-03-03 00:11:31 +0400

    02.03.26 "Работа с json-файлами. Часть 2" Задача 16 "Общественное питание 😰"

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import json
 
-with open('food_services.json', 'r', encoding='utf-8') as inp_file:
-    reader = json.load(inp_file)
+with open('food_services.json', 'r', encoding='utf-8') as file:
+    reader = json.load(file)
 
-d, d2 = {}, {}
+# 1
+d = {}
 for i in reader:
-    d[i['District']] = d.get(i['District'], 0) + 1
-    if i['OperatingCompany'] == '':
-        continue
-    d2[i['OperatingCompany']] = d2.get(i['OperatingCompany'], 0) + 1
+    d.setdefault(i['TypeObject'], []).append((i['Name'], i['SeatsCount']))
 
-max_d = sorted(d.items(), key=lambda x: x[1], reverse=True)[0]  # max_d = max(d.items(), key=lambda x: x[1])
-max_d2 = sorted(d2.items(), key=lambda x: x[1], reverse=True)[0]  # max_d = max(d, key=d.get)
+for i in sorted(d):
+    max_place = max(d[i], key=lambda x: x[1])  # max_place = sorted(d[i], key=lambda x: x[1], reverse=True)[0]
+    print(f"{i}: {max_place[0]}, {max_place[1]}")
 
-print(f"{max_d[0]}: {max_d[1]}")
-print(f"{max_d2[0]}: {max_d2[1]}")
+# 2
+d = {i['TypeObject']: f"{i['Name']}, {i['SeatsCount']}" for i in
+     sorted(reader, key=lambda x: (x['TypeObject'], x['SeatsCount']))}
+for i in d.items():
+    print(f"{i[0]}: {i[1]}")
