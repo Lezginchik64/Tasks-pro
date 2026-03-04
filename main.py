c9c34c831e29532c1ed7645c6612803ee26ac180
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-03-05 02:56:57 +0400

    04.03 "Работа с zip-файлами" Задача 5 "Форматированный вывод"

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from zipfile import ZipFile
 import datetime as dt
 
-# 1
+
 with ZipFile('workbook.zip', 'r') as file:
     info = file.infolist()
-    d = (2021, 11, 30, 14, 22)
-    dates = [i.filename.split('/')[-1] for i in info if i.date_time > d and not i.is_dir()]
-    print(*sorted(dates), sep='\n')
-
-# 2
-dates = [info[i].filename.split('/')[-1] for i in range(len(info)) if
-         dt.datetime(*info[i].date_time) > dt.datetime(*d) and not info[i].is_dir()]
+    for i in sorted(info, key=lambda x: x.filename.split('/')[-1]):
+        if not i.is_dir():
+            print(i.filename.split('/')[-1])
+            print(f'  Дата модификации файла: {dt.datetime(*i.date_time)}')
+            print(f'  Объем исходного файла: {i.file_size} байт(а)')
+            print(f'  Объем сжатого файла: {i.compress_size} байт(а)')
+            print()
