75f72d951aeaf6e336ce539d03346b36c9c7bba0
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-03-04 14:48:42 +0400

    04.03 "Работа с zip-файлами" Задача 3 "Наилучший показатель"

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from zipfile import ZipFile
 # 1
 with ZipFile('workbook.zip', 'r') as file:
     info = file.infolist()
-    size = sum(i.file_size for i in info)
-    compress_size = sum(i.compress_size for i in info)
-    print(f'Объем исходных файлов: {size} байт(а)')
-    print(f'Объем сжатых файлов: {compress_size} байт(а)')
+    min_file = min((i for i in info if i.file_size), key=lambda x: (x.compress_size / x.file_size) * 100)
+    print(min_file.filename.split('/')[1])
 
 # 2
-with ZipFile('workbook.zip') as myzip:
-    a, b = map(sum, zip(*[(f.file_size, f.compress_size) for f in myzip.infolist()]))
-    print(f'Объем исходных файлов: {a} байт(а)')
-    print(f'Объем сжатых файлов: {b} байт(а)')
+with ZipFile('workbook.zip', 'r') as file:
+    info = file.infolist()
+    k = {ind: (i.compress_size / i.file_size) * 100 for ind, i in enumerate(info) if i.file_size}
+    min_k = min(k, key=k.get)
+    print(info[min_k].filename.split('/')[1])
