9ce48385508a38afcfc36f786aa98465b4227868
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-03-04 12:39:02 +0400

    04.03 "Работа с zip-файлами" Задача 2 "Объем файлов"

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from zipfile import ZipFile
 
 # 1
 with ZipFile('workbook.zip', 'r') as file:
-    files = [i for i in file.infolist() if not i.is_dir()]
-    print(len(files))
+    info = file.infolist()
+    size = sum(i.file_size for i in info)
+    compress_size = sum(i.compress_size for i in info)
+    print(f'Объем исходных файлов: {size} байт(а)')
+    print(f'Объем сжатых файлов: {compress_size} байт(а)')
 
 # 2
-with ZipFile('workbook.zip', 'r') as file:
-    files = sum(not i.is_dir() for i in file.infolist())
-    print(files)
+with ZipFile('workbook.zip') as myzip:
+    a, b = map(sum, zip(*[(f.file_size, f.compress_size) for f in myzip.infolist()]))
+    print(f'Объем исходных файлов: {a} байт(а)')
+    print(f'Объем сжатых файлов: {b} байт(а)')
