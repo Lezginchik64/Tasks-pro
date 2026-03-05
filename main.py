640c9f44cf69fd1f40dd8e1b5991fb253e949c1e
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-03-05 23:50:49 +0400

    05.03 "Работа с zip-файлами" Задача 8 "Функция extract_this()" !

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 from zipfile import ZipFile
-import os.path
-
-file_names = ['how to prove.pdf', 'fipi_demo_2022.pdf', 'Hollow Knight Silksong.exe',
-              'code.jpeg', 'stepik.png', 'readme.txt', 'shopping_list.txt',
-              'Alexandra Savior – Crying All the Time.mp3', 'homework.py', 'test.py']
-
-# 1
-with ZipFile('files.zip', 'w') as file:
-    [file.write(i) for i in file_names if os.path.getsize(i) <= 100]
-# Получить объем файла в байтах позволяет функция getsize() из модуля os.path.
-# Данная функция принимает в качестве аргумента путь к файлу и возвращает размер указанного файла в байтах.
 
 
-# 2
-with ZipFile('files.zip', 'w') as zip_file:
-    for f in file_names:
-        with open(f, 'rb') as bite_file:  # rb - открывает файл в режиме «только для чтения» в двоичном формате (исп для pdf, mp3, png и тд)
-            data = bite_file.read(101)  # считываем только до 101 байта, если больше, то не считывается (эффективнее на больших файлах)
-            size = len(data)
-            if size <= 100:
-                zip_file.write(f)
+def extract_this(zip_name, *args):
+    with ZipFile(zip_name, 'r') as zip_file:
+        zip_file.extractall(members=args or None)   # если args - пустой, тогда None (members=None - извлечь все файлы)
+
+# С методом extractall() используем необязательный аргумент members. По умолчанию members = None.
+# Если members = None, то извлекутся все файлы в архиве.
+# Если в members передать список файлов, то извлекутся конкретные файлы.
+
+extract_this('workbook.zip')
