761c386298ab7dcab4c12cbb4d7b07de447cbd3f
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-03-26 03:12:04 +0400

    25.03 "Тип данных defaultdict" Задача 6 "Функция best_sender()"

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 from collections import defaultdict
 
+# 1
+def best_sender(messages, senders):
+    res = defaultdict(int)
+    for i in range(len(messages)):
+        res[senders[i]] += len(messages[i].split())
+    return max(res, key=lambda x: (res[x], x))
 
-def flip_dict(dict_of_lists):
-    res = defaultdict(list)
-    for key, val in dict_of_lists.items():
-        for elem in val:
-            res[elem].append(key)
-    return res
+# 2
+def best_sender(messages, senders):
+    counts = defaultdict(int)
+    for sender, message in zip(senders, messages):
+        counts[sender] += len(message.split())
+    return max(counts, key=lambda sender: (counts[sender], sender))
 
 
-print(flip_dict({'a': [1, 2], 'b': [3, 1], 'c': [2]}))
+messages = ['Hi, Linda', 'Hi, Sam', 'How are you doing?']
+senders = ['Sam Fisher', 'Linda', 'Sam Fisher']
+print(best_sender(messages, senders))
+
+messages = ['Hello userTwooo', 'Hi userThree', 'Wonderful day Alice', 'Nice day userThree']
+senders = ['Alice', 'userTwo', 'userThree', 'Alice']
+print(best_sender(messages, senders))
+
+messages = ['thanks', 'Stepik is useful for', 'thanks', 'np ur welcome', 'practice']
+senders = ['Bob', 'Charlie', 'Bob', 'Bob', 'Charlie']
+print(best_sender(messages, senders))
+
+messages = ['How is Stepik for everyone', 'Stepik is useful for practice']
+senders = ['Bob', 'Charlie']
+print(best_sender(messages, senders))
