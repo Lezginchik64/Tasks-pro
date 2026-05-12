7a6fcf8f724cf3cfc9d8bbf3438059efc983e021
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-05-13 01:29:25 +0400

    13.05 "Тип данных Counter. Часть 2" Задача 2 "В поисках слов 😋"

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from collections import Counter
 
-inp = input().lower().split()
-print(Counter(inp).most_common()[0][0])
+# 1
+counts = Counter(input().lower().split())
+min_count = min(counts.values())
+print(*sorted(filter(lambda x: counts[x] == min_count, counts)), sep=', ')
+
+# 2
+words = input().lower().split()
+counts = Counter(words)
+min_count = min(counts.values())
+res = sorted(key for key, value in counts.items() if value == min_count)
+print(', '.join(res))
