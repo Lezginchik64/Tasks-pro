e38f7e01cec03a753b30892ea784f8645bb74cce
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-05-13 01:35:10 +0400

    13.05 "Тип данных Counter. Часть 2" Задача 3

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from collections import Counter
 
-# 1
 counts = Counter(input().lower().split())
-min_count = min(counts.values())
-print(*sorted(filter(lambda x: counts[x] == min_count, counts)), sep=', ')
-
-# 2
-words = input().lower().split()
-counts = Counter(words)
-min_count = min(counts.values())
-res = sorted(key for key, value in counts.items() if value == min_count)
-print(', '.join(res))
+max_count = max(counts.values())
+print(sorted(filter(lambda x: counts[x] == max_count, counts), reverse=True)[0])
