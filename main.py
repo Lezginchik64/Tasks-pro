bffcba75618d229a6a181009bc28df05aafbd834
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-05-20 02:31:14 +0400

    20.05 "Тип данных Counter. Часть 2" Задача 4 "Статистика длин слов"

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from collections import Counter
 
-counts = Counter(input().lower().split())
-max_count = max(counts.values())
-print(sorted(filter(lambda x: counts[x] == max_count, counts), reverse=True)[0])
+words = map(len, input().split())
+counts = Counter(words)
+for k, v in sorted(counts.items(), key=lambda x: x[1]):
+    print(f'Слов длины {k}: {v}')
